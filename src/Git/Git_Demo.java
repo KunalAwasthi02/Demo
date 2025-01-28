6494f3c2dda739d951fbf2810fcf9d9ea23c20e0
Kunal <kunal.awasthi@brevitaz.com>
2025-01-28 16:56:15 +0530

    Checking git 1st time
    Signed-off-by: Kunal <kunal.awasthi@brevitaz.com>

---
+++ src/Git/Git_Demo.java
@@ -0,0 +1,10 @@
+package Git;
+// I will check the
+// how-to implements basic functionality of git
+public class Git_Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+
+}
